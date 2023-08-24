06b0a86dc5e198bd0ed512651d89666390358d74
Denis Mironchuk <dmiro@softserveinc.com>
2023-08-25 00:42:07 +0300

    UC#1 Create sort function by name

--- src/main/java/genAIuc1/CountriesController.java
+++ src/main/java/genAIuc1/CountriesController.java
@@ -9,10 +9,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 @RestController
 public class CountriesController {
 
     public static final String COUNTRIES_URL = "https://restcountries.com/v3.1/all";
+    public static final String NAME_PATH = "/name/common";
 
     private ObjectMapper objectMapper = new ObjectMapper();
     private RestTemplate templ = new RestTemplate();
@@ -20,7 +25,7 @@ public class CountriesController {
     @GetMapping("/getCountries")
     public String getCountries(@RequestParam(required = false) String countryName,
                                @RequestParam(required = false) Integer population,
-                               @RequestParam(required = false) String param3,
+                               @RequestParam(required = false) SortDirection sort,
                                @RequestParam(required = false) String param4) throws JsonProcessingException {
         String response = templ.getForObject(COUNTRIES_URL, String.class);
         JsonNode result = objectMapper.readTree(response);
@@ -30,13 +35,16 @@ public class CountriesController {
         if (population != null) {
             result = filterByPopulation(population, result);
         }
+        if (sort != null) {
+            result = sortByName(sort, result);
+        }
         return result.toString();
     }
 
     private JsonNode filterByCountryName(String searchCountryName, JsonNode rootNode) {
         ArrayNode filteredNodes = objectMapper.createArrayNode();
         for (JsonNode countryNode : rootNode) {
-            String countryName = countryNode.at("/name/common").asText();
+            String countryName = countryNode.at(NAME_PATH).asText();
             if (countryName != null && countryName.toLowerCase().contains(searchCountryName.toLowerCase())) {
                 filteredNodes.add(countryNode);
             }
@@ -53,5 +61,23 @@ public class CountriesController {
             }
         }
         return filteredNodes;
+    }
+
+    private JsonNode sortByName(SortDirection direction, JsonNode rootNode) {
+        ArrayNode sortedNodes = objectMapper.createArrayNode();
+        List<JsonNode> nodes = new ArrayList<>();
+        for (JsonNode countryNode : rootNode) {
+            nodes.add(countryNode);
+        }
+        Comparator<JsonNode> nameComparator = Comparator.comparing(node -> node.at(NAME_PATH).asText());
+        if (direction == SortDirection.descend) {
+            nameComparator = nameComparator.reversed();
+        }
+        nodes.stream().sorted(nameComparator).forEach(sortedNodes::add);
+        return sortedNodes;
+    }
+
+    private enum SortDirection {
+        ascend, descend
     }
 }
