4f22e982a9299df37f98da59789ce565cef9b417
Denis Mironchuk <dmiro@softserveinc.com>
2023-08-25 00:24:27 +0300

    UC#1 Create filter by population function

--- src/main/java/genAIuc1/CountriesController.java
+++ src/main/java/genAIuc1/CountriesController.java
@@ -9,35 +9,46 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Iterator;
-
 @RestController
 public class CountriesController {
 
     public static final String COUNTRIES_URL = "https://restcountries.com/v3.1/all";
 
     private ObjectMapper objectMapper = new ObjectMapper();
+    private RestTemplate templ = new RestTemplate();
 
     @GetMapping("/getCountries")
-    public String getCountries(@RequestParam(required = false) String countryName, @RequestParam(required = false) String param2,
-                               @RequestParam(required = false) String param3, @RequestParam(required = false) String param4) throws JsonProcessingException {
-        String url = COUNTRIES_URL;
-        RestTemplate templ = new RestTemplate();
-        String response = templ.getForObject(url, String.class);
+    public String getCountries(@RequestParam(required = false) String countryName,
+                               @RequestParam(required = false) Integer population,
+                               @RequestParam(required = false) String param3,
+                               @RequestParam(required = false) String param4) throws JsonProcessingException {
+        String response = templ.getForObject(COUNTRIES_URL, String.class);
         JsonNode result = objectMapper.readTree(response);
         if (countryName != null) {
             result = filterByCountryName(countryName, result);
+        }
+        if (population != null) {
+            result = filterByPopulation(population, result);
         }
         return result.toString();
     }
 
     private JsonNode filterByCountryName(String searchCountryName, JsonNode rootNode) {
-        Iterator<JsonNode> nodesItr = rootNode.elements();
         ArrayNode filteredNodes = objectMapper.createArrayNode();
-        while (nodesItr.hasNext()) {
-            JsonNode countryNode = nodesItr.next();
+        for (JsonNode countryNode : rootNode) {
             String countryName = countryNode.at("/name/common").asText();
             if (countryName != null && countryName.toLowerCase().contains(searchCountryName.toLowerCase())) {
+                filteredNodes.add(countryNode);
+            }
+        }
+        return filteredNodes;
+    }
+
+    private JsonNode filterByPopulation(Integer searchPopulation, JsonNode rootNode) {
+        ArrayNode filteredNodes = objectMapper.createArrayNode();
+        for (JsonNode countryNode : rootNode) {
+            int population = countryNode.at("/population").asInt(Integer.MAX_VALUE);
+            if (population < searchPopulation * 1000000) {
                 filteredNodes.add(countryNode);
             }
         }
