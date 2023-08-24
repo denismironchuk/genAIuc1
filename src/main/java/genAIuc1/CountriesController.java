7f4b81e7fce52d12ffc4b4343c9711fe4142f802
Denis Mironchuk <dmiro@softserveinc.com>
2023-08-25 00:12:48 +0300

    “UC#1 Create filter function by name

--- src/main/java/genAIuc1/CountriesController.java
+++ src/main/java/genAIuc1/CountriesController.java
@@ -3,6 +3,7 @@ package genAIuc1;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,14 +16,31 @@ public class CountriesController {
 
     public static final String COUNTRIES_URL = "https://restcountries.com/v3.1/all";
 
+    private ObjectMapper objectMapper = new ObjectMapper();
+
     @GetMapping("/getCountries")
-    public String getCountries(@RequestParam(required = false) String param1, @RequestParam(required = false) String param2,
+    public String getCountries(@RequestParam(required = false) String countryName, @RequestParam(required = false) String param2,
                                @RequestParam(required = false) String param3, @RequestParam(required = false) String param4) throws JsonProcessingException {
         String url = COUNTRIES_URL;
         RestTemplate templ = new RestTemplate();
         String response = templ.getForObject(url, String.class);
-        ObjectMapper objectMapper = new ObjectMapper();
-        JsonNode rootNode = objectMapper.readTree(response);
-        return "SUCCESS!";
+        JsonNode result = objectMapper.readTree(response);
+        if (countryName != null) {
+            result = filterByCountryName(countryName, result);
+        }
+        return result.toString();
+    }
+
+    private JsonNode filterByCountryName(String searchCountryName, JsonNode rootNode) {
+        Iterator<JsonNode> nodesItr = rootNode.elements();
+        ArrayNode filteredNodes = objectMapper.createArrayNode();
+        while (nodesItr.hasNext()) {
+            JsonNode countryNode = nodesItr.next();
+            String countryName = countryNode.at("/name/common").asText();
+            if (countryName != null && countryName.toLowerCase().contains(searchCountryName.toLowerCase())) {
+                filteredNodes.add(countryNode);
+            }
+        }
+        return filteredNodes;
     }
 }
